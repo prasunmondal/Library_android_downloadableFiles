3c8c7438661da78815374d6064e32e31433a8909
Prasun <pmandal@akamai.com>
2020-08-14 21:18:42 +0530

    working fine

--- app/src/main/java/com/prasunmondal/lib/android/downloadablefile/MainActivity.kt
+++ app/src/main/java/com/prasunmondal/lib/android/downloadablefile/MainActivity.kt
@@ -8,5 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val metadata = DownloadableFiles(
+            applicationContext,
+            "https://docs.google.com/spreadsheets/d/e/2PACX-1vRZQ28x7jpdIOzT2PA6iTCTcyTHM9tVPkv2ezuqd4LFOWu9SJqImGM7ML8ejdQB01SdjfTZnoHogzUt/pub?gid=1321322233&single=true&output=csv",
+            "",
+            "metadata.csv",
+            "E203",
+            "fetching metadata"
+        )
+
+        metadata.download(applicationContext, {})
     }
 }
